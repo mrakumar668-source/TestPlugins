3181984895b7378b01dc667426f3f9a9cde952e2
mrakumar668-source <mrakumar668@gmail.com>
2026-07-05 19:23:51 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,13 +2,6 @@ plugins {
     id("com.github.recloudstream.gradle") version "1.0.0"
 }
 
-repositories {
-    mavenCentral()
-    maven {
-        url = uri("https://maven.pkg.github.com/recloudstream/gradle")
-    }
-}
-
 cloudstream {
     language = "en"
     description = "Watch anime from Miruro.tv"
